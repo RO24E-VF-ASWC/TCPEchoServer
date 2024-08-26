892f807c21c629479b2cbc63fff82eb5b613feef
Peter Levinsky <pele@zealand.dk>
2024-08-26 15:10:31 +0200

    Add project files.

---
+++ TCPEchoClient/EchoClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPEchoClient
+{
+    public class EchoClient
+    {
+        public EchoClient() { }
+        public void Start()
+        {
+            using (TcpClient socket = new TcpClient("localhost", 7))
+            using (StreamReader sr = new StreamReader(socket.GetStream()))
+            using (StreamWriter sw = new StreamWriter(socket.GetStream()))
+            {
+                string str = "I'll send this message";
+
+                sw.WriteLine(str);
+                sw.Flush();
+
+                String? inStr = sr.ReadLine();
+                Console.WriteLine(inStr);
+            }
+
+        }
+    }
+}
